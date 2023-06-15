2e9c21db32721c932d12c86605fda8b424e2f24b
sameer <sameersultan1001@gmail.com>
2023-06-15 16:43:30 +0500

    UI complete

--- sign-in.js
+++ sign-in.js
@@ -3,8 +3,44 @@ console.log("hello world");
 let title_container= document.querySelector('#title-container');
 let form_container= document.querySelector('#form-container');
 let title_inner= document.querySelector('.signIn_title');
-console.log(title_inner);
+let signup_form= document.querySelector('#signup-form');
+let signIn_form= document.querySelector('#signIn_form');
+let signin_content= document.querySelector('#signin-content');
+let signup_content= document.querySelector('#signup-content');
+// sign up input field
+let signup_btn= document.querySelector('#signup-btn');
+let signup_email= document.querySelector('#signup-email');
+let signup_user= document.querySelector('#signup-user');
+let signup_password= document.querySelector('#signup-Password');
+let signup_confirm_password= document.querySelector('#signup-confirm-password');
+
+
+
 function sign_up_click(){
     title_inner.classList.toggle("order-2");
-    preventDefault();
+    form_container.classList.replace('rounded-start','rounded-end');
+    title_container.classList.replace('rounded-end','rounded-start');
+    signIn_form.classList.toggle('d-none');
+    signup_form.classList.toggle('d-none');
+    signin_content.classList.toggle('d-none');
+    signup_content.classList.toggle('d-none');
+    
+}
+
+signup_btn.addEventListener('click',function(e){
+    e.preventDefault();
+    let credientails_storage=localStorage.getItem('credientails_storage');
+    if(credientails_storage === null){
+        let credientails_list=[];
+    }
+    else{
+        credientails_list = JSON.parse(credientails_storage); 
+    }
+
+    valid(signup_email.value,signup_password.value);  
+ 
+})
+
+function valid(email,password){
+    
 }
